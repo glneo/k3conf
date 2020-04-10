c9fe76b1f28e08612b357f60e15d8d9a56b5b044
Lokesh Vutla <lokeshvutla@ti.com>
2020-04-10 16:22:48 +0530

    soc: am65x_sr2: Add clocks information
    Add TISCI clock information for AM65x SR2 devices. Data derived from
    sysfw 2020.02 Documentation.
    
    Tested-by: Dave Gerlach <d-gerlach@ti.com>
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- common/socinfo.c
+++ common/socinfo.c
@@ -46,6 +46,7 @@
 #include <soc/am65x_sr2/am65x_sr2_sec_proxy_info.h>
 #include <soc/am65x_sr2/am65x_sr2_processors_info.h>
 #include <soc/am65x_sr2/am65x_sr2_devices_info.h>
+#include <soc/am65x_sr2/am65x_sr2_clocks_info.h>
 #include <soc/j721e/j721e_host_info.h>
 #include <soc/j721e/j721e_sec_proxy_info.h>
 #include <soc/j721e/j721e_processors_info.h>
@@ -122,6 +123,8 @@ static void am654_sr2_init(void)
 	sci_info->num_processors = AM65X_SR2_MAX_PROCESSORS_IDS;
 	sci_info->devices_info = am65x_sr2_devices_info;
 	sci_info->num_devices = AM65X_SR2_MAX_DEVICES;
+	sci_info->clocks_info = am65x_sr2_clocks_info;
+	sci_info->num_clocks = AM65X_SR2_MAX_CLOCKS;
 }
 
 static void j721e_init(void)
