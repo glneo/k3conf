9ad5e4f750145defdd5cac20c2edb4ea796e654b
Lokesh Vutla <lokeshvutla@ti.com>
2020-07-10 10:23:33 +0530

    soc: j7200: Add Devices info
    Add TISCI device information for J7200 devices.
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- common/socinfo.c
+++ common/socinfo.c
@@ -55,6 +55,7 @@
 #include <soc/j7200/j7200_host_info.h>
 #include <soc/j7200/j7200_sec_proxy_info.h>
 #include <soc/j7200/j7200_processors_info.h>
+#include <soc/j7200/j7200_devices_info.h>
 
 /* Assuming these addresses and definitions stay common across K3 devices */
 #define CTRLMMR_WKUP_JTAG_DEVICE_ID	0x43000018
@@ -154,6 +155,8 @@ static void j7200_init(void)
 	sci_info->num_sp_threads[MCU_SEC_PROXY] = J7200_MCU_SEC_PROXY_THREADS;
 	sci_info->processors_info = j7200_processors_info;
 	sci_info->num_processors = J7200_MAX_PROCESSORS_IDS;
+	sci_info->devices_info = j7200_devices_info;
+	sci_info->num_devices = J7200_MAX_DEVICES;
 }
 
 int soc_init(uint32_t host_id)
