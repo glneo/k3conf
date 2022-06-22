glneo/k3conf

commit 2434dcb46bc74ef6a1f1323210cfcb07d7b71407

soc: j721s2 add processor information
The Processor IDs (not to be confused with Host IDs) represent the
actual physical processors in the J721S2.

Provide the processor and host IDs available for the J721S2

Signed-off-by: Bryan Brattlof <bb@ti.com>

common/socinfo.c
@@ -76,6 +76,7 @@
 #include <soc/j721s2/j721s2_devices_info.h>
 #include <soc/j721s2/j721s2_clocks_info.h>
 #include <soc/j721s2/j721s2_host_info.h>
+#include <soc/j721s2/j721s2_processors_info.h>
 
 /* Assuming these addresses and definitions stay common across K3 devices */
 #define CTRLMMR_WKUP_JTAG_DEVICE_ID	0x43000018

Makefile
@@ -121,7 +121,8 @@ AM62XSOURCES =\
 J721S2SOURCES =\
 	      soc/j721s2/j721s2_devices_info.c \
 	      soc/j721s2/j721s2_clocks_info.c \
-	      soc/j721s2/j721s2_host_info.c
+	      soc/j721s2/j721s2_host_info.c \
+	      soc/j721s2/j721s2_processors_info.c
 
 COMMONOBJECTS=	$(COMMONSOURCES:.c=.o)
 AM65XOBJECTS=	$(AM65XSOURCES:.c=.o)

soc/j721s2/j721s2_processors_info.c
@@ -1,0 +1,49 @@
+/*
+ * J721S2 Processor Info
+ *
+ * Copyright (C) 2022 Texas Instruments Incorporated - https://www.ti.com/
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <tisci.h>
+#include <socinfo.h>
+
+struct ti_sci_processors_info j721s2_processors_info[] = {
+	[0] = {202, 0, 0x20, "A72SS0_CORE0"},
+	[1] = {203, 0, 0x21, "A72SS0_CORE1"},
+	[2] = {8, 0, 0x30, "COMPUTE_CLUSTER0_C71SS0_0"},
+	[3] = {11, 0, 0x31, "COMPUTE_CLUSTER0_C71SS1_0"},
+	[4] = {284, 0, 0x01, "MCU_R5FSS0_CORE0"},
+	[5] = {285, 0, 0x02, "MCU_R5FSS0_CORE1"},
+	[6] = {279, 0, 0x06, "R5FSS0_CORE0"},
+	[7] = {280, 0, 0x07, "R5FSS0_CORE1"},
+	[8] = {281, 0, 0x08, "R5FSS1_CORE0"},
+	[9] = {282, 0, 0x09, "R5FSS1_CORE1"},
+};

soc/j721s2/j721s2_processors_info.h
@@ -1,0 +1,41 @@
+/*
+ * J721S2 Processor Info
+ *
+ * Copyright (C) 2022 Texas Instruments Incorporated - https://www.ti.com/
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __J721S2_PROCESSOR_INFO_H
+#define __J721S2_PROCESSOR_INFO_H
+
+#define J721S2_MAX_PROCESSORS_IDS	10
+extern struct ti_sci_processors_info j721s2_processors_info[];
+
+#endif /* __J721S2_PROCESSOR_INFO_H */

commit 48cb8e8d7dc962229a9f318cd512b4ddeec5cdc5

soc: j721s2: set processor information
While the processor information for J721S2 is already populated, it is
not set in sci_info. As a result, "k3conf dump processors" does not show
anything. Set processors_info and num_processors to correctly dump all
processors.

Fixes: 2434dcb ("soc: j721s2 add processor information")
Signed-off-by: Pratyush Yadav <p.yadav@ti.com>
Signed-off-by: Bryan Brattlof <bb@ti.com>

common/socinfo.c
@@ -172,6 +172,8 @@ static void j721s2_init(void)
 	sci_info->num_clocks = J721S2_MAX_CLOCKS;
 	sci_info->devices_info = j721s2_devices_info;
 	sci_info->num_devices = J721S2_MAX_DEVICES;
+	sci_info->processors_info = j721s2_processors_info;
+	sci_info->num_processors = J721S2_MAX_PROCESSORS_IDS;
 	soc_info.host_id = DEFAULT_HOST_ID;
 	soc_info.sec_proxy = &k3_generic_sec_proxy_base;
 }