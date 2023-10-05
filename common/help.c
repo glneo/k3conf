80be8381cfa0753c1d1a710119f38992ade6ac5f
Andrew Davis <afd@ti.com>
2023-10-05 09:59:31 -0500

    common: help: Remove leading newline and tabs escape sequences
    These can make it harder to see what the output will look like from
    only looking at the code. They also confuse tools like codespell.
    
    Move leading newlines to a new line. Replace tab escape sequences
    with real tabs.
    
    Should result in no functional change to the help output.
    
    Signed-off-by: Andrew Davis <afd@ti.com>

--- common/help.c
+++ common/help.c
@@ -48,169 +48,200 @@ void help(help_category cat)
 	}
 
 	if ((cat == HELP_ALL) || (cat == HELP_USAGE)) {
-		printf("\nNAME\n");
-		printf("\tk3conf - TI K3 Configuration Diagnostic Tool\n");
-
-		printf("\nSYNOPSIS\n");
-		printf("\tk3conf [--host <id>] [--version] [--help] [--cpuinfo] <command> [<args>]\n");
-		if (cat == HELP_USAGE)
-			printf("\n\tSee 'k3conf --help' for more information.\n\n");
+		printf("\n");
+		printf("NAME\n");
+		printf("	k3conf - TI K3 Configuration Diagnostic Tool\n");
+		printf("\n");
+		printf("SYNOPSIS\n");
+		printf("	k3conf [--host <id>] [--version] [--help] [--cpuinfo] <command> [<args>]\n");
+		if (cat == HELP_USAGE) {
+			printf("\n");
+			printf("	See 'k3conf --help' for more information.\n");
+			printf("\n");
+		}
 	}
 
 	if (cat == HELP_ALL) {
-		printf("\nDESCRIPTION\n");
-		printf("\tk3conf is standalone application designed to provide"
-			" a quick'n easy way to \n\tdiagnose/debug/audit"
-			" TI K3 architecture based processors configuration"
-			" at\n\truntime, with no particular kernel dependency.\n");
-		printf("\tk3conf is designed to be as much platform-agnostic"
-			" as possible, being able to \n\trun on any Linux platform"
-			" and easily ported to other OS.\n");
-		printf("\tEven if k3conf today focuses mainly on TISCI"
-			" related functionality, it is \n\tintended to be extended"
-			" to any other area.\n");
+		printf("\n");
+		printf("DESCRIPTION\n");
+		printf("	k3conf is standalone application designed to provide a quick'n easy way to \n"
+			"	diagnose/debug/audit TI K3 architecture based processors configuration at\n"
+			"	runtime, with no particular kernel dependency.\n");
+		printf("	k3conf is designed to be as much platform-agnostic as possible, being able to \n"
+			"	run on any Linux platform and easily ported to other OS.\n");
+		printf("	Even if k3conf today focuses mainly on TISCI related functionality, it is \n"
+			"	intended to be extended to any other area.\n");
 
-		printf("\nOPTIONS\n");
-		printf("\t--host <id>\n");
-		printf("\t    Use this host id for communicating with sysfw\n\n");
-
-		printf("\t--help\n");
-		printf("\t    Print k3conf help.\n");
-
-		printf("\n\t--version\n");
-		printf("\t    Print k3conf version.\n");
-
-		printf("\n\t--cpuinfo\n");
-		printf("\t    Print the host processor information.\n");
+		printf("\n");
+		printf("OPTIONS\n");
+		printf("	--host <id>\n");
+		printf("	    Use this host id for communicating with sysfw\n");
+		printf("\n");
+		printf("	--help\n");
+		printf("	    Print k3conf help.\n");
+		printf("\n");
+		printf("	--version\n");
+		printf("	    Print k3conf version.\n");
+		printf("\n");
+		printf("	--cpuinfo\n");
+		printf("	    Print the host processor information.\n");
 	}
 
-	if (cat != HELP_USAGE)
-		printf("\nCOMMANDS\n");
+	if (cat != HELP_USAGE) {
+		printf("\n");
+		printf("COMMANDS\n");
+	}
 
 	if ((cat == HELP_ALL) || (cat == HELP_SHOW) ||
 	    (cat == HELP_SHOW_HOST)) {
-		printf("\n\tk3conf show hosts\n");
-		printf("\t	Prints all the available TISCI hosts\n");
+		printf("\n");
+		printf("	k3conf show hosts\n");
+		printf("		Prints all the available TISCI hosts\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_SHOW) ||
 	    (cat == HELP_SHOW_SEC_PROXY)) {
-		printf("\n\tk3conf show threads\n");
-		printf("\t	Prints all the available TISCI secure proxy threads\n");
+		printf("\n");
+		printf("	k3conf show threads\n");
+		printf("		Prints all the available TISCI secure proxy threads\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_SHOW) ||
 	    (cat == HELP_SHOW_DEVICE)) {
-		printf("\n\tk3conf show device\n");
-		printf("\t	Prints all the available TISCI devices\n");
-		printf("\n\tk3conf show device <dev_id>\n");
-		printf("\t	Prints the corresponding device id information\n");
+		printf("\n");
+		printf("	k3conf show device\n");
+		printf("		Prints all the available TISCI devices\n");
+		printf("\n");
+		printf("	k3conf show device <dev_id>\n");
+		printf("		Prints the corresponding device id information\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_SHOW) ||
 	    (cat == HELP_SHOW_CLOCK)) {
-		printf("\n\tk3conf show clock\n");
-		printf("\t	Prints all the available TISCI clocks\n");
-		printf("\n\tk3conf show clock <dev_id>\n");
-		printf("\t	Prints the clocks for corresponding device id\n");
+		printf("\n	k3conf show clock\n");
+		printf("		Prints all the available TISCI clocks\n");
+		printf("\n	k3conf show clock <dev_id>\n");
+		printf("		Prints the clocks for corresponding device id\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_SHOW) ||
 	    (cat == HELP_SHOW_PROCESSOR)) {
-		printf("\n\tk3conf show processor\n");
-		printf("\t	Prints all the available TISCI processors\n");
+		printf("\n");
+		printf("	k3conf show processor\n");
+		printf("		Prints all the available TISCI processors\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_SHOW) ||
 	    (cat == HELP_SHOW_RM)) {
-		printf("\n\tk3conf show rm\n");
-		printf("\t	Prints resources managed by System firmware\n");
-		printf("\n\tk3conf show rm <dev_id>\n");
-		printf("\t	Prints resources managed by System firmware for corresponding device\n");
+		printf("\n");
+		printf("	k3conf show rm\n");
+		printf("		Prints resources managed by System firmware\n");
+		printf("\n");
+		printf("	k3conf show rm <dev_id>\n");
+		printf("		Prints resources managed by System firmware for corresponding device\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_DUMP) ||
 	    (cat == HELP_DUMP_DEVICE)) {
-		printf("\n\tk3conf dump device\n");
-		printf("\t	Prints device status of all the TISCI devices\n");
-		printf("\n\tk3conf dump device <dev_id>\n");
-		printf("\t	Prints the corresponding device id status\n");
+		printf("\n");
+		printf("	k3conf dump device\n");
+		printf("		Prints device status of all the TISCI devices\n");
+		printf("\n");
+		printf("	k3conf dump device <dev_id>\n");
+		printf("		Prints the corresponding device id status\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_DUMP) ||
 	    (cat == HELP_DUMP_CLOCK)) {
-		printf("\n\tk3conf dump clock\n");
-		printf("\t	Prints clock status all the available TISCI clocks\n");
-		printf("\n\tk3conf dump clock <dev_id>\n");
-		printf("\t	Prints the available clock status for corresponding device id\n");
+		printf("\n");
+		printf("	k3conf dump clock\n");
+		printf("		Prints clock status all the available TISCI clocks\n");
+		printf("\n");
+		printf("	k3conf dump clock <dev_id>\n");
+		printf("		Prints the available clock status for corresponding device id\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_DUMP) ||
 	    (cat == HELP_DUMP_CLOCK_PARENT)) {
-		printf("\n\tk3conf dump parent_clock <dev_id> <clk_id>\n");
-		printf("\t	Prints the clock parent of provided clock\n");
+		printf("\n");
+		printf("	k3conf dump parent_clock <dev_id> <clk_id>\n");
+		printf("		Prints the clock parent of provided clock\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_DUMP) ||
 	    (cat == HELP_DUMP_PROCESSOR)) {
-		printf("\n\tk3conf dump processor\n");
-		printf("\t	Prints status of all the available TISCI processors\n");
-		printf("\n\tk3conf dump processor <proc_id>\n");
-		printf("\t	Prints status of the given TISCI processors\n");
+		printf("\n");
+		printf("	k3conf dump processor\n");
+		printf("		Prints status of all the available TISCI processors\n");
+		printf("\n");
+		printf("	k3conf dump processor <proc_id>\n");
+		printf("		Prints status of the given TISCI processors\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_DUMP) ||
 	    (cat == HELP_DUMP_RM)) {
-		printf("\n\tk3conf dump rm [OPTIONS]\n");
-		printf("\t	Prints resource allocation for all utypes / hosts\n");
-		printf("\n\tk3conf dump rm [OPTIONS] <type>\n");
-		printf("\t	Prints resource allocation for corresponding type\n");
-		printf("\n\tk3conf dump rm [OPTIONS] <type> <subtype>\n");
-		printf("\t	Prints resource allocation for corresponding device/type\n");
-		printf("\t	\n");
-		printf("\t	[OPTIONS]\n");
-		printf("\t	-h <host_id>\n");
-		printf("\t		Filter only for corresponding host_id\n");
+		printf("\n");
+		printf("	k3conf dump rm [OPTIONS]\n");
+		printf("		Prints resource allocation for all utypes / hosts\n");
+		printf("\n");
+		printf("	k3conf dump rm [OPTIONS] <type>\n");
+		printf("		Prints resource allocation for corresponding type\n");
+		printf("\n");
+		printf("	k3conf dump rm [OPTIONS] <type> <subtype>\n");
+		printf("		Prints resource allocation for corresponding device/type\n");
+		printf("\n");
+		printf("		[OPTIONS]\n");
+		printf("		-h <host_id>\n");
+		printf("			Filter only for corresponding host_id\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_ENABLE) ||
 	    (cat == HELP_ENABLE_DEVICE)) {
-		printf("\n\tk3conf enable device <dev_id>\n");
-		printf("\t	Enables the TISCI device and prints the status\n");
+		printf("\n");
+		printf("	k3conf enable device <dev_id>\n");
+		printf("		Enables the TISCI device and prints the status\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_ENABLE) ||
 	    (cat == HELP_ENABLE_CLOCK)) {
-		printf("\n\tk3conf enable clock <dev_id> <clk_id>\n");
-		printf("\t	Enables the TISCI clock and prints the status\n");
+		printf("\n");
+		printf("	k3conf enable clock <dev_id> <clk_id>\n");
+		printf("		Enables the TISCI clock and prints the status\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_DISABLE) ||
 	    (cat == HELP_DISABLE_DEVICE)) {
-		printf("\n\tk3conf disable device <dev_id>\n");
-		printf("\t	Disables the TISCI device and prints the status\n");
+		printf("\n");
+		printf("	k3conf disable device <dev_id>\n");
+		printf("		Disables the TISCI device and prints the status\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_DISABLE) ||
 	    (cat == HELP_DISABLE_CLOCK)) {
-		printf("\n\tk3conf disable clock <dev_id> <clk_id>\n");
-		printf("\t	Disables the TISCI clock and prints the status\n");
+		printf("\n");
+		printf("	k3conf disable clock <dev_id> <clk_id>\n");
+		printf("		Disables the TISCI clock and prints the status\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_SET) ||
 	    (cat == HELP_SET_CLOCK)) {
-		printf("\n\tk3conf set clock <dev_id> <clk_id> <freq>\n");
-		printf("\t	Sets the clock frequency and prints the status\n");
+		printf("\n");
+		printf("	k3conf set clock <dev_id> <clk_id> <freq>\n");
+		printf("		Sets the clock frequency and prints the status\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_SET) ||
 	    (cat == HELP_SET_CLOCK_PARENT)) {
-		printf("\n\tk3conf set parent_clock <dev_id> <clk_id> <parent_clk_id>\n");
-		printf("\t	Sets the parent clock for a clock mux and prints the mux status\n");
-		printf("\t	Refer to the following documentation for preconditions:\n");
-		printf("\t	%s\n", HELP_CLK_SET_PARENT_URL1);
-		printf("\t	%s\n", HELP_CLK_SET_PARENT_URL2);
+		printf("\n");
+		printf("	k3conf set parent_clock <dev_id> <clk_id> <parent_clk_id>\n");
+		printf("		Sets the parent clock for a clock mux and prints the mux status\n");
+		printf("		Refer to the following documentation for preconditions:\n");
+		printf("		%s\n", HELP_CLK_SET_PARENT_URL1);
+		printf("		%s\n", HELP_CLK_SET_PARENT_URL2);
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_READ)) {
-		printf("\n\tk3conf read <addr> [<size>]\n");
-		printf("\t	No.of bits to be read is given in the size argument\n");
-		printf("\t	Expected input size is 8,16,32,64\n");
-		printf("\t	Prints the value at the specified io memory\n");
+		printf("\n");
+		printf("	k3conf read <addr> [<size>]\n");
+		printf("		No.of bits to be read is given in the size argument\n");
+		printf("		Expected input size is 8,16,32,64\n");
+		printf("		Prints the value at the specified io memory\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_WRITE)) {
-		printf("\n\tk3conf write <addr> <val> [<size>]\n");
-		printf("\t	No.of bits to be written is given in the size argument\n");
-		printf("\t	Expected input size is 8,16,32,64\n");
-		printf("\t	Writes the value at the specified io memory\n");
+		printf("\n");
+		printf("	k3conf write <addr> <val> [<size>]\n");
+		printf("		No.of bits to be written is given in the size argument\n");
+		printf("		Expected input size is 8,16,32,64\n");
+		printf("		Writes the value at the specified io memory\n");
 	}
 	if ((cat == HELP_ALL) || (cat == HELP_DUMP_DDRBW)) {
-		printf("\n\tk3conf ddrbw <sample_duration> <count>\n");
-		printf("\t	Prints DDR bandwidth utilization on all DDR instances\n");
-		printf("\t	sample_duration is delay between samples in seconds (default: 1 second)\n");
-		printf("\t	count is number of samples to capture before stopping (default: -1 infinite)\n");
+		printf("\n");
+		printf("	k3conf ddrbw <sample_duration> <count>\n");
+		printf("		Prints DDR bandwidth utilization on all DDR instances\n");
+		printf("		sample_duration is delay between samples in seconds (default: 1 second)\n");
+		printf("		count is number of samples to capture before stopping (default: -1 infinite)\n");
 	}
 }
