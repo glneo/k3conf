6c8025b7aa322dbc8c4981ac5afee4b14a70b8ea
Andrew Davis <afd@ti.com>
2024-09-17 14:19:15 -0500

    common: ddr-perf: Clearify output as Mebibyte vs Megabytes
    Signed-off-by: Andrew Davis <afd@ti.com>

--- common/cmd_ddr_perf.c
+++ common/cmd_ddr_perf.c
@@ -126,10 +126,10 @@ int ddrbw_info(int argc, char *argv[])
 
 	autoadjust_table_init(table);
 	strncpy(table[row][0], "DDR instance", TABLE_MAX_ELT_LEN);
-	strncpy(table[row][1], "Read data (MB)", TABLE_MAX_ELT_LEN);
-	strncpy(table[row][2], "Avg Read B/W (MB/s)", TABLE_MAX_ELT_LEN);
-	strncpy(table[row][3], "Write data (MB)", TABLE_MAX_ELT_LEN);
-	strncpy(table[row][4], "Avg Write B/W (MB/s)", TABLE_MAX_ELT_LEN);
+	strncpy(table[row][1], "Read data (MiB)", TABLE_MAX_ELT_LEN);
+	strncpy(table[row][2], "Avg Read B/W (MiB/s)", TABLE_MAX_ELT_LEN);
+	strncpy(table[row][3], "Write data (MiB)", TABLE_MAX_ELT_LEN);
+	strncpy(table[row][4], "Avg Write B/W (MiB/s)", TABLE_MAX_ELT_LEN);
 
 	while (auto_refresh--) {
 		struct timespec timer;
@@ -157,19 +157,17 @@ int ddrbw_info(int argc, char *argv[])
 			uint64_t time = 0;
 			float read_bw = 0, write_bw = 0;
 
-			float read_bytes = (float)read_count * pinfo->burst_size;
-			float write_bytes = (float)write_count * pinfo->burst_size;
+			float read_mibytes = ((float)read_count * pinfo->burst_size) / (1024 * 1024);
+			float write_mibytes = ((float)write_count * pinfo->burst_size) / (1024 * 1024);
 
 			time = TO_USEC(dcap[i].last_time) - TO_USEC(dcap[i].first_time);
-			read_bw = read_bytes / time;
-			write_bw = write_bytes / time;
+			read_bw = read_mibytes / time;
+			write_bw = write_mibytes / time;
 
 			snprintf(table[i + 1][0], TABLE_MAX_ELT_LEN, "DDR%d", i);
-			snprintf(table[i + 1][1], TABLE_MAX_ELT_LEN, "%.3f",
-				 read_bytes / 1000000u);
+			snprintf(table[i + 1][1], TABLE_MAX_ELT_LEN, "%.3f", read_mibytes);
 			snprintf(table[i + 1][2], TABLE_MAX_ELT_LEN, "%.3f", read_bw);
-			snprintf(table[i + 1][3], TABLE_MAX_ELT_LEN, "%.3f",
-				 write_bytes / 1000000u);
+			snprintf(table[i + 1][3], TABLE_MAX_ELT_LEN, "%.3f", write_mibytes);
 			snprintf(table[i + 1][4], TABLE_MAX_ELT_LEN, "%.3f", write_bw);
 		}
 		ret = autoadjust_table_print(table, 1 + pinfo->num_perf_insts, 5);
