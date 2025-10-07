362c5677578cafa7e57a162c1a066bdf700b74c9
Andrew Davis <afd@ti.com>
2025-10-07 14:25:02 -0500

    common: ddr-perf: Aggregate data accross all DDR instances
    Signed-off-by: Andrew Davis <afd@ti.com>

--- common/cmd_ddr_perf.c
+++ common/cmd_ddr_perf.c
@@ -58,52 +58,42 @@ int ddrbw_info(int argc, char *argv[])
 	}
 
 	autoadjust_table_init(table);
-	strncpy(table[row][0], "DDR instance", TABLE_MAX_ELT_LEN);
-	strncpy(table[row][1], "Read data (MiB)", TABLE_MAX_ELT_LEN);
-	strncpy(table[row][2], "Avg Read B/W (MiB/s)", TABLE_MAX_ELT_LEN);
-	strncpy(table[row][3], "Write data (MiB)", TABLE_MAX_ELT_LEN);
-	strncpy(table[row][4], "Avg Write B/W (MiB/s)", TABLE_MAX_ELT_LEN);
-
-	char *events = "";
-	for (int i = 0; i < pinfo->num_perf_insts; i++) {
-		char *name;
-		asprintf(&name, "k3_ddr_%lx", (pinfo->perf_inst_base[i]) >> 12);
-		asprintf(&events, "%s%s/read/,%s/write/,", events, name, name);
-		free(name);
-	}
-	events[strlen(events) - 1] = '\0'; // Zap last ','
+	strncpy(table[row][0], "Read data (MiB)", TABLE_MAX_ELT_LEN);
+	strncpy(table[row][1], "Avg Read B/W (MiB/s)", TABLE_MAX_ELT_LEN);
+	strncpy(table[row][2], "Write data (MiB)", TABLE_MAX_ELT_LEN);
+	strncpy(table[row][3], "Avg Write B/W (MiB/s)", TABLE_MAX_ELT_LEN);
 
 	char *command;
-	asprintf(&command, "/root/perf stat -e %s -a -I %d --interval-count %d -x ';' 2>&1", events, duration * 1000, auto_refresh);
+	asprintf(&command, "/root/perf stat -e k3_ddr/read/,k3_ddr/write/ -a -I %d --interval-count %d -x ';' 2>&1", duration * 1000, auto_refresh);
 	FILE *fp = popen(command, "r");
 	free(command);
 
 	while (true) {
-		for (int i = 0; i < pinfo->num_perf_insts; i++) {
-			uint32_t read_count, write_count;
-			uint64_t read_time, write_time;
+		uint32_t read_count, write_count;
+		uint64_t read_time, write_time;
 
-			/* Read count and time */
-			if (!parse_line(fp, &read_count, &read_time)) goto out;
+		/* Read count and time */
+		if (!parse_line(fp, &read_count, &read_time)) break;
+		read_time /= pinfo->num_perf_insts; // Un-aggregate time
 
-			/* Write count and time */
-			if (!parse_line(fp, &write_count, &write_time)) goto out;
+		/* Write count and time */
+		if (!parse_line(fp, &write_count, &write_time)) break;
+		write_time /= pinfo->num_perf_insts; // Un-aggregate time
 
-			float read_mibytes = ((float)read_count * pinfo->burst_size) / (1024 * 1024);
-			float write_mibytes = ((float)write_count * pinfo->burst_size) / (1024 * 1024);
+		float read_mibytes = ((float)read_count * pinfo->burst_size) / (1024 * 1024);
+		float write_mibytes = ((float)write_count * pinfo->burst_size) / (1024 * 1024);
 
-			float read_bw = read_mibytes / (read_time / 1000000000.0f);
-			float write_bw = write_mibytes / (write_time / 1000000000.0f);
+		float read_bw = read_mibytes / (read_time / 1000000000.0f);
+		float write_bw = write_mibytes / (write_time / 1000000000.0f);
 
-			snprintf(table[i + 1][0], TABLE_MAX_ELT_LEN, "DDR%d", i);
-			snprintf(table[i + 1][1], TABLE_MAX_ELT_LEN, "%.3f", read_mibytes);
-			snprintf(table[i + 1][2], TABLE_MAX_ELT_LEN, "%.3f", read_bw);
-			snprintf(table[i + 1][3], TABLE_MAX_ELT_LEN, "%.3f", write_mibytes);
-			snprintf(table[i + 1][4], TABLE_MAX_ELT_LEN, "%.3f", write_bw);
-		}
-		ret = autoadjust_table_print(table, 1 + pinfo->num_perf_insts, 5);
+		snprintf(table[1][0], TABLE_MAX_ELT_LEN, "%.3f", read_mibytes);
+		snprintf(table[1][1], TABLE_MAX_ELT_LEN, "%.3f", read_bw);
+		snprintf(table[1][2], TABLE_MAX_ELT_LEN, "%.3f", write_mibytes);
+		snprintf(table[1][3], TABLE_MAX_ELT_LEN, "%.3f", write_bw);
+
+		ret = autoadjust_table_print(table, 2, 5);
 	}
-out:
+
 	pclose(fp);
 
 	return ret;
